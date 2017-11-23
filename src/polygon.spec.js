12dec36ca18e6422695050bd3ad9823ebb11afaf
kinogam <kinogam@kinogamdeMacBook-Pro.local>
2017-11-23 15:52:31 +0800

    add files

---
+++ src/polygon.spec.js
@@ -0,0 +1,9 @@
+import Polygon from 'polygon';
+
+describe('ES6 Polygon', function() {
+    let polygon = new Polygon(5, 4);
+
+    it('should return 20 when calling calcArea', function() {
+        expect(polygon.calcArea()).toBe(20);
+    });
+});
